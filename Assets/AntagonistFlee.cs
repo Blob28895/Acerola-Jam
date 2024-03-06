5a0a64ab3edbba38bf372eb7886e2275e749e7b4
Blob28895 <blob28895@gmail.com>
2024-03-05 22:18:24 -0600

    added a fleeing antagonist

---
+++ Assets/AntagonistFlee.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AntagonistFlee : MonoBehaviour
+{
+
+    [SerializeField] private ParticleSystem particles;
+	[SerializeField] private SpriteRenderer spriteRenderer;
+
+	private void Awake()
+	{
+		particles.Stop();
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if(collision.gameObject.CompareTag("Player") && spriteRenderer.enabled)
+		{
+			StartCoroutine(flee());
+		}
+	}
+
+	private IEnumerator flee()
+	{
+		particles.Play();
+		spriteRenderer.enabled = false;
+		yield return new WaitForSeconds(0.5f);
+		particles.Stop();
+	}
+
+}
