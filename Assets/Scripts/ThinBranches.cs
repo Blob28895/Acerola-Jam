54917d1f96f444e2034eb9c874a3d514fcb45d4b
Blob28895 <blob28895@gmail.com>
2024-03-08 19:45:07 -0600

    added thin branches break particles

--- Assets/Scripts/ThinBranches.cs
+++ Assets/Scripts/ThinBranches.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.ParticleSystem;
 
 public class ThinBranches : MonoBehaviour
 {
+	[SerializeField] private ParticleSystem particles;
+	[SerializeField] private SpriteRenderer spriteRenderer;
+	[SerializeField] private BoxCollider2D boxCollider;
+
+	private void Awake()
+	{
+		particles.Stop();
+	}
 
 	private void OnCollisionEnter2D(Collision2D collision)
 	{
 		if(collision.gameObject.CompareTag("Player"))
 		{
-			if (collision.gameObject.GetComponent<PlayerProgression>().canBreak()) { 
-			gameObject.SetActive(false);
+			if (collision.gameObject.GetComponent<PlayerProgression>().canBreak())
+			{
+				boxCollider.enabled = false;
+				StartCoroutine(demolish());
 
 			}
 		}
+	}
+
+	private IEnumerator demolish()
+	{
+		spriteRenderer.enabled = false;
+		particles.Play();
+		spriteRenderer.enabled = false;
+		yield return new WaitForSeconds(0.5f);
+		particles.Stop(true, ParticleSystemStopBehavior.StopEmitting);
 	}
 }
