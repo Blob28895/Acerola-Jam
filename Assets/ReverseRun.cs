a0bcaef493a8ae04d24f0813e2ad8f340289f152
Blob28895 <blob28895@gmail.com>
2024-03-11 19:51:16 -0500

    added rocks to block the basic path back in level 4

--- Assets/ReverseRun.cs
+++ Assets/ReverseRun.cs
@@ -1,13 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.ParticleSystem;
 
 public class ReverseRun : MonoBehaviour
 {
 	[SerializeField] private GameObject level4BackwardsTransition;
+	[SerializeField] private GameObject level4Rocks;
 	public void StartReverseRun()
 	{
 		level4BackwardsTransition.SetActive(true);
+		StartCoroutine(rockFall());
 		GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerProgression>().activateWings();
+		
+	}
+
+	private IEnumerator rockFall()
+	{
+		ParticleSystem particles = level4Rocks.GetComponent<ParticleSystem>();
+		level4Rocks.SetActive(true);
+		particles.Play();
+		yield return new WaitForSeconds(0.25f);
+		particles.Stop(true, ParticleSystemStopBehavior.StopEmitting);
 	}
 }
